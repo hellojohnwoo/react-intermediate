8b7445281fce849fd02e1381cc9ab663b5cc97f4
hellojohnwoo <hellojohnwoo@gmail.com>
2020-08-21 13:37:18 +0900

    0821 todo app recheck

--- hello-react-practice/chap10_todo_app/src/App.js
+++ hello-react-practice/chap10_todo_app/src/App.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function App() {
+// function App() {
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'react1',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'react2',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'react3',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
-}
+};
 
 export default App;
